b4af70fb7555e240e7e991a2f04759d7dc634654
fabio.aoliveira <fabio.aoliveira@dataprev.gov.br>
2023-09-04 13:30:53 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import 'bootswatch/dist/flatly/bootstrap.css'
+
+
+function App() {
+
+  return (
+      <div>
+        <h1>Olá Mundo!</h1>
+        <button type="button" className="btn btn-primary">Clique Aqui</button>
+      </div>
+  )
+}
+
+export default App
